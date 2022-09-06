569d179c8e417dbd49d0fb3c43e94ca2dccf09f2
asus <asus@LAPTOP-PGBN799U>
2022-09-06 23:07:07 +0300

    commit

---
+++ kucuktenBuyuge/src/kucuktenBuyuge.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class kucuktenBuyuge {
+
+	public static void main(String[] args) {
+		int a, b, c;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("1.sayýyý giriniz:");
+		a = input.nextInt();
+		System.out.println("2.sayýyý giriniz:");
+		b = input.nextInt();
+		System.out.println("3.sayýyý giriniz:");
+		c = input.nextInt();
+
+		if (a < b && a < c) {
+			if (b < c) {
+				System.out.println("a<b<c");
+			} 
+			else {
+				System.out.println("a<c<b");
+			}
+		} else if (b < a && b < c) {
+			if (a < c) {
+				System.out.println("b<a<c");
+			} 
+			else {
+				System.out.println("b<c<a");
+			}
+		} else {
+			if (a < b) {
+				System.out.println("c<a<b");
+			} 
+			else {
+				System.out.println("c<b<a");
+			}
+		}
+	}
+}
